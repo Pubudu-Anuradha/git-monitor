d683aab8cd08386066a7bd3315da235b1bee11ab
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2023-07-26 00:03:57 +0530

    Added Update Repo and misc cleanup

--- git-monitor-backend/src/services/repos/mod.rs
+++ git-monitor-backend/src/services/repos/mod.rs
@@ -13,10 +13,11 @@ pub fn repos() -> actix_web::Scope {
     .service(web::resource("").to(get_all))
     .service(web::resource("/update").to(update_repos))
     .service(web::resource("/all").to(get_all))
-    .service(web::resource("/get").to(get_repo))
-  // .service(web::resource("").to(|| HttpResponse::Ok()))
-  // .service(web::resource("/scanlocal").to(scan_local_repos))
-  // .service(web::resource("/repo").to(get_repo))
+    .service(
+      web::resource("/repo")
+        .route(web::get().to(get_repo))
+        .route(web::post().to(set_repo)),
+    )
 }
 
 async fn update_repos() -> Json<Vec<String>> {
@@ -224,149 +225,44 @@ pub async fn get_repo(
   )
 }
 
-// #[derive(Serialize)]
-// pub struct Repo {
-//   pub name: String,
-//   pub dir: String,
-//   pub is_valid: bool,
-//   pub state: String,
-//   pub statuses: Vec<Status>,
-//   pub branches: Vec<Branch>,
-// }
+#[derive(serde::Deserialize)]
+pub struct SetRepoRequest {
+  #[allow(dead_code)]
+  path: String,
+  #[allow(dead_code)]
+  absolute: bool,
+  #[allow(dead_code)]
+  managed: bool,
+}
 
-// #[derive(Serialize)]
-// pub struct Branch {
-//   name: String,
-//   branch_type: String,
-//   is_head: bool,
-//   upstream: String,
-// }
-
-// #[derive(Serialize)]
-// pub struct Status {
-//   pub status: String,
-//   pub path: String,
-// }
-
-// impl Repo {
-//   fn new(name: String, dir: String) -> Self {
-//     match Repository::open(Path::new(dir.as_str())) {
-//       Ok(repo) => {
-//         print!("");
-//         let mut options = StatusOptions::new();
-//         options
-//           .include_ignored(false)
-//           .include_unmodified(false)
-//           .include_untracked(true);
-//         let branches = Vec::from_iter(
-//           repo
-//             .branches(None)
-//             .unwrap()
-//             .map(|b| Branch::new(b.unwrap())),
-//         );
-//         Self {
-//           name,
-//           dir,
-//           is_valid: true,
-//           state: utils::state_to_string(repo.state()),
-//           statuses: Vec::from_iter(
-//             repo
-//               .statuses(Some(&mut options))
-//               .unwrap()
-//               .into_iter()
-//               .map(|s| utils::status(s)),
-//           ),
-//           branches,
-//         }
-//       }
-//       Err(_) => Self {
-//         name,
-//         dir,
-//         is_valid: false,
-//         state: "invalid".to_string(),
-//         statuses: Vec::new(),
-//         branches: Vec::new(),
-//       },
-//     }
-//   }
-// }
-
-// impl Branch {
-//   fn new(b: (git2::Branch, BranchType)) -> Self {
-//     let branch = b.0;
-//     let upstream = match branch.upstream() {
-//       Ok(u) => u.name().unwrap().unwrap().to_string(),
-//       Err(_) => "".to_string(),
-//     };
-//     Self {
-//       name: branch.name().unwrap().unwrap().to_string(),
-//       branch_type: match b.1 {
-//         BranchType::Local => "Local",
-//         BranchType::Remote => "Remote",
-//       }
-//       .to_string(),
-//       is_head: branch.is_head(),
-//       upstream,
-//     }
-//   }
-// }
-
-// #[derive(Deserialize)]
-// pub struct RepoRequest {
-//   #[allow(dead_code)]
-//   path: String,
-//   #[allow(dead_code)]
-//   absolute: bool,
-// }
-
-// pub async fn get_repo(req: web::Json<RepoRequest>) -> Json<Repo> {
-//   let path = req
-//     .path
-//     .as_str()
-//     .trim_end_matches(" ")
-//     .trim_end_matches("/");
-//   let name = path[path.rfind("/").unwrap_or(0) + 1..path.len()].to_string();
-
-//   Json(Repo::new(
-//     name,
-//     match req.absolute {
-//       true => path.to_string(),
-//       false => format!(
-//         "{}/{}",
-//         get_home_dir(),
-//         path
-//           .trim_start_matches(" ")
-//           .trim_start_matches("~")
-//           .trim_start_matches("/")
-//       ),
-//     },
-//   ))
-// }
-
-// pub async fn scan_local_repos() -> Json<Vec<Repo>> {
-//   let home_dir = get_home_dir();
-//   let root = Path::new(home_dir.as_str());
-//   let repos = Vec::from_iter(
-//     WalkDir::new(root)
-//       .follow_links(false)
-//       .into_iter()
-//       .filter(|f| {
-//         f.as_ref().is_ok_and(|d| {
-//           d.file_type().is_dir() && d.file_name().eq_ignore_ascii_case(".git")
-//         })
-//       })
-//       .map(|f| f.unwrap())
-//       .map(|dir_entry| {
-//         let dir = dir_entry
-//           .path()
-//           .parent()
-//           .unwrap_or(root)
-//           .to_string_lossy()
-//           .to_string();
-//         // Trimming the prefix from the path
-//         let name = dir[dir.rfind("/").unwrap_or(0) + 1..dir.len()].to_string();
-//         Repo::new(name, dir)
-//       }),
-//   );
-//   web::Json(repos)
-// }
+async fn set_repo(
+  req: web::Json<SetRepoRequest>,
+) -> Json<Result<repo::Data, prisma_client_rust::QueryError>> {
+  let path = req
+    .path
+    .as_str()
+    .trim_end_matches(" ")
+    .trim_end_matches("/");
+  let abs_path = match req.absolute {
+    true => path.to_string(),
+    false => format!(
+      "{}/{}",
+      get_home_dir(),
+      path
+        .trim_start_matches(" ")
+        .trim_start_matches("~")
+        .trim_start_matches("/")
+    ),
+  };
+  let conn = get_prisma_connection().await;
+  Json(
+    conn
+      .repo()
+      .update(
+        prisma::repo::UniqueWhereParam::DirEquals(abs_path),
+        vec![prisma::repo::managed::set(req.managed)],
+      )
+      .exec()
+      .await,
+  )
+}
